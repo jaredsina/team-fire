70b30cbbed34ec9980e4dda0bff6f97d2681a20d
Shritant <shritan06@gmail.com>
2026-02-02 18:03:50 -0500

    NPC Movement

--- Assets/NPCMovement.cs
+++ Assets/NPCMovement.cs
@@ -3,18 +3,36 @@ using UnityEngine.AI; // Required for NavMesh features
 
 public class NPCMovement : MonoBehaviour
 {
+    public Transform player;
     public NavMeshAgent agent;
 
     void Update()
     {
+        agent.SetDestination(player.position);
         // Continuously update the destination to the target's position
-        if(Input.GetMouseButtonDown(1))
+        // 
+        //         if(Input.GetMouseButtonDown(1))
+        //         {
+        //             Ray movePosition = Camera.main.ScreenPointToRay(new Vector3(100,100,100));
+        //             if (Physics.Raycast(movePosition, out var hitInfo))
+        //             {
+        //                 agent.SetDestination(hitInfo.point);
+        //             }
+        //         }
+        // 
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Player")
         {
-            Ray movePosition = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(movePosition, out var hitInfo))
-            {
-                agent.SetDestination(hitInfo.point);
-            }
+            Destroy(gameObject);
+            Debug.Log("Hit: ");
         }
     }
 }
+
+
+    
+
+
