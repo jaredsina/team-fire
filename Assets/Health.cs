ac492b9b0ac44853af3737a04c987d870132b3f6
Ruhan Dutta <ruhan122011@gmail.com>
2026-02-02 17:50:46 -0500

    Range Weapons
    Made the banana and grape grenade. Added their movement and the grapes explosion particles. Also added damage for grape and the banana, when the dummies health goes to 0 the dummy disappears.

---
+++ Assets/Health.cs
@@ -0,0 +1,41 @@
+using Unity.Collections;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Health : MonoBehaviour
+{
+    public float maxHealth = 100f;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Banana")
+        {
+            Debug.Log("banana");
+            maxHealth -= 5;
+            Debug.Log(maxHealth);
+        }
+
+        if (collision.gameObject.tag == "Grape")
+        {
+            Debug.Log("grape");
+            maxHealth -= 40;
+            Debug.Log(maxHealth);
+
+        }
+        if (maxHealth == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
