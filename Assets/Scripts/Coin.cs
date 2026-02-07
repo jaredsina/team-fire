df8b7e5c1dfd39fe2404574a1310256d399d9ae9
SiddharthP711 <sid711p@gmail.com>
2026-02-07 13:36:34 -0500

    Coin counter
    Created a counter that adds one coin per collection.

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -1,14 +1,17 @@
 using UnityEngine;
 using System.Collections;
 
+
 public class Coin : MonoBehaviour
 {
     private Renderer renderer;
+    private GameObject gameManager;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         renderer = GetComponent<Renderer>();
+        gameManager = GameObject.Find("GameManager");
     }
 
     // Update is called once per frame
@@ -21,6 +24,7 @@ public class Coin : MonoBehaviour
     {
         if (other.CompareTag("Player") && renderer.enabled){
             renderer.enabled = false;
+            gameManager.GetComponent<GoldScript>().ChangeGold(1);
             StartCoroutine(Respawn());
         }
     }
