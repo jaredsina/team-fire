59a45767c5ee3b3ad5ce4da21ff2f28ec1ee4f08
Nico38714 <nicolas.d.braem@gmail.com>
2026-02-14 14:02:29 -0500

    Continued working on hotbar, added selected slot image and connected slots to scripts

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public int selectedSlot = 0;
+    public string[] inventory = {"", ""};
+
+    public GameObject slot1;
+    public GameObject slot2;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
