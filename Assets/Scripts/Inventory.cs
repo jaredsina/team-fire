19c627fdde2ab0101e4d643eb25e4763cb2e550b
Nico38714 <nicolas.d.braem@gmail.com>
2026-02-28 13:38:54 -0500

    Added hotbar and added selected and unselected slots

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
@@ -8,15 +10,34 @@ public class Inventory : MonoBehaviour
     public GameObject slot1;
     public GameObject slot2;
 
+    public Image image1;
+    public Image image2;
+
+    public Sprite selectedSprite;
+    public Sprite unselectedSprite;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        image1 = slot1.GetComponent<Image>();
+        image2 = slot2.GetComponent<Image>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Keyboard.current.digit1Key.wasPressedThisFrame)
+        {
+            selectedSlot = 0;
+            image1.sprite = selectedSprite;
+            image2.sprite = unselectedSprite;
+        }
+
+        if (Keyboard.current.digit2Key.wasPressedThisFrame)
+        {
+            selectedSlot = 1;
+            image2.sprite = selectedSprite;
+            image1.sprite = unselectedSprite;
+        }
     }
 }
